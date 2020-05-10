72de08f98c408973b58110845974bdfb9338c42e
chrylZzz <cyl5yy412@gmail.com>
2020-05-10 09:07:11 +0800

    Zzz

--- src/main/java/com/chryl/domain/EsProduct.java
+++ src/main/java/com/chryl/domain/EsProduct.java
@@ -29,6 +29,17 @@ import java.util.List;
  * Attachment,
  * Keyword  //不会进行分词建立索引的类型
  */
+//@Document 作用在类，标记实体类为文档对象，一般有四个属性
+//        indexName：对应索引库名称
+//        type：对应在索引库中的类型
+//        shards：分片数量，默认5
+//        replicas：副本数量，默认1
+//@Id 作用在成员变量，标记一个字段作为id主键
+//@Field 作用在成员变量，标记为文档的字段，并指定字段映射属性：
+//        type：字段类型，取值是枚举：FieldType
+//        index：是否索引，布尔类型，默认是true
+//        store：是否存储，布尔类型，默认是false
+//        analyzer：分词器名称：ik_max_word
 @Document(indexName = "pms", type = "product", shards = 1, replicas = 0)
 public class EsProduct implements Serializable {
     private static final long serialVersionUID = -1L;
